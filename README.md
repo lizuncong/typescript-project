a5c10e8b94b6dcbe45b1e60cdfd52257860c0f38
lizuncong <2900170896@qq.com>
2021-03-31 09:35:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### 运行
+ts代码不能直接运行，需要转换成js代码。
+- `npx tsc demo.ts` 运行 `tsc` 命令编译ts文件，生成demo.js文件，再运行 `node demo.js`
+- 或者，安装 `ts-node` 插件，然后可以直接运行 `npx ts-node demo.ts`
---
+++ demo.ts
@@ -0,0 +1,17 @@
+
+
+const count: number = 2019
+
+// interface用来定义对象类型
+interface Point {
+    x: number; // 这里好像逗号，分号都可以
+    y: number,
+    z: number;
+}
+
+
+const point: Point = {
+    x: 12,
+    y: 3,
+    z: 6
+}
