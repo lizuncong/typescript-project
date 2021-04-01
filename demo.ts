8e42fbdac339a34bdbe61625c454711eb62f0821
lizuncong <2900170896@qq.com>
2021-04-01 15:59:44 +0800

    类的定义和继承

--- demo.ts
+++ demo.ts
@@ -1,11 +1,17 @@
-
-interface Print {
-    (content: string): string;
-    // name: string;
-}
-const print1: Print = (content) => {
-    console.log('print content', content);
-    return content
+class Person{
+    constructor(public name:string){}
 }
 
-print1('my name is lzc')
+class Student extends Person{
+    constructor(private age: number, name: string){
+        // 一定要手动调用父类的构造器
+        super(name);
+    }
+    printInfo(){
+        console.log('my name is：', this.name);
+        console.log('my age is：', this.age);
+    }
+}
+
+const stu = new Student(26,'lzc');
+stu.printInfo();
