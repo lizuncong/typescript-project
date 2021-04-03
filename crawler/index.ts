3f97e7c47929e902d6233370fc50b4ca0bc28b3f
lizuncong <2900170896@qq.com>
2021-04-03 22:28:51 +0800

    爬虫小项目

--- crawler/index.ts
+++ crawler/index.ts
@@ -1,40 +1,38 @@
 import superagent from 'superagent';
 import cheerio from 'cheerio';
+import fs from 'fs';
+import path from 'path';
+import CourseAnalyzer from './courseAnalyzer';
 
-interface Course {
-    level: string;
-    count: number;
+export interface Analyzer {
+    analyze: (html: string, filePath: string) => string;
 }
 
 class Crawler {
-    // 要爬取的网页URL
-    private url = 'https://coding.imooc.com/?c=fe';
+    private filePath = path.resolve(__dirname, './data/course.json');
 
-    constructor(){
-        this.getRawHtml();
+    constructor(private analyzer: Analyzer, private url: string){
+        this.initSpiderProcess();
     }
 
-    async getRawHtml(){
+    private async initSpiderProcess() {
+        const html = await this.getRawHtml();
+        const fileContent = this.analyzer.analyze(html, this.filePath);
+        this.writeFile(fileContent)
+    }
+
+    private async getRawHtml(){
         const { text } = await superagent.get(this.url);
-        this.getCourseInfo(text);
+        return text;
     }
 
-    getCourseInfo(html: string){
-        const $ = cheerio.load(html);
-        const courseCards = $('.course-card');
-        const courseInfos: Course[] = [];
-        courseCards.map((index, element) => {
-            const desc = $(element).find('.one span');
-            const [level, count] = desc.text().split('·');
-            courseInfos.push({
-                level,
-                count: parseInt(count),
-            })
-        })
-
-        console.log(courseInfos);
+    private writeFile(content: string){
+        fs.writeFileSync(this.filePath, content)
     }
 }
 
+// 要爬取的网页URL
+const url = 'https://coding.imooc.com/?c=fe';
 
-const crawler = new Crawler();
+const courseAnalyzer = CourseAnalyzer.getInstance();
+const crawler = new Crawler(courseAnalyzer, url);
