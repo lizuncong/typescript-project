b02165d75d6242c34a36bee4a42ee119b19a5df6
lizuncong <2900170896@qq.com>
2021-04-05 18:17:31 +0800

    方法的装饰器

--- demo.js
+++ demo.js
@@ -1,17 +1,4 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -21,28 +8,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-function personDecorator(constructor) {
-    return (function (_super) {
-        __extends(class_1, _super);
-        function class_1() {
-            var _this = _super !== null && _super.apply(this, arguments) || this;
-            _this.name = '张三';
-            return _this;
-        }
-        class_1.prototype.getName = function () {
-            return this.name;
-        };
-        return class_1;
-    }(constructor));
+function getNameDecorator(target, key, descriptor) {
+    console.log('target..', target);
+    console.log('key..', key);
+    descriptor.writable = false;
+    descriptor.value = function () {
+        return 'descriptor.value';
+    };
 }
 var Person = (function () {
     function Person(name) {
         this.name = name;
     }
-    Person = __decorate([
-        personDecorator,
-        __metadata("design:paramtypes", [String])
-    ], Person);
+    Person.prototype.getName = function () {
+        return this.name;
+    };
+    __decorate([
+        getNameDecorator,
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", []),
+        __metadata("design:returntype", void 0)
+    ], Person.prototype, "getName", null);
     return Person;
 }());
 var p = new Person('lzc');
--- demo.ts
+++ demo.ts
@@ -3,24 +3,54 @@
 // 首先 (...args: any[]) => any 是一个函数，函数返回一个any表示函数返回值是任意类型，
 // 函数接受很多个参数...args。
 // 因此new (...args: any[]) => any 表示一个构造函数
-function personDecorator(){
-    return function<T extends new (...args: any[]) => any>(constructor: T){
-        return class extends constructor {
-            name = '张三';
-            getName(){
-                return this.name;
-            }
-        };
+// function personDecorator(){
+//     return function<T extends new (...args: any[]) => any>(constructor: T){
+//         return class extends constructor {
+//             name = '张三';
+//             getName(){
+//                 return this.name;
+//             }
+//         };
+//     }
+// }
+//
+//
+//
+// const Person = personDecorator()(class{
+//     constructor(public name: string){
+//     }
+// })
+
+// 普通方法，target对应的是类的prototype
+// key 对应的是装饰的方法的名字
+function getNameDecorator(target: any, key: string, descriptor: PropertyDescriptor) {
+    console.log('target..', target);
+    console.log('key..', key);
+    descriptor.writable = false; //装饰的方法不能被重写
+    descriptor.value = () => {
+        return 'descriptor.value';
     }
 }
 
-
-
-const Person = personDecorator()(class{
-    constructor(public name: string){
+class Person {
+    name: string;
+    constructor(name: string){
+        this.name = name;
     }
-})
+
+    @getNameDecorator
+    getName(){
+        return this.name;
+    }
+
+    // @getNameDecorator
+    // static staticGetName(){
+    //     return '张三'
+    // }
+}
 
 const p = new Person('lzc');
+
+
 console.log('p..', p);
 console.log('p.getName..', p.getName());
