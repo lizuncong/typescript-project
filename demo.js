4eef762737090d8cfc828c55036949bc1bbeb340
lizuncong <2900170896@qq.com>
2021-04-05 15:15:46 +0800

    类的装饰器

--- demo.js
+++ demo.js
@@ -1,32 +1,50 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function testDecorator(constructor) {
-    constructor.prototype.getName = function () {
-        console.log('lzc');
-    };
-    console.log('testDecorator.....', constructor);
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+function personDecorator(constructor) {
+    return (function (_super) {
+        __extends(class_1, _super);
+        function class_1() {
+            var _this = _super !== null && _super.apply(this, arguments) || this;
+            _this.name = '张三';
+            return _this;
+        }
+        class_1.prototype.getName = function () {
+            return this.name;
+        };
+        return class_1;
+    }(constructor));
 }
-function testDecorator2(flag) {
-    if (!flag) {
-        return function (constructor) { };
+var Person = (function () {
+    function Person(name) {
+        this.name = name;
     }
-    return function testDecorator2(constructor) {
-        console.log('testDecorator2222.....', constructor);
-    };
-}
-var Test = (function () {
-    function Test() {
-    }
-    Test = __decorate([
-        testDecorator,
-        testDecorator2(false)
-    ], Test);
-    return Test;
+    Person = __decorate([
+        personDecorator,
+        __metadata("design:paramtypes", [String])
+    ], Person);
+    return Person;
 }());
-var test = new Test();
-test.getName();
+var p = new Person('lzc');
+console.log('p..', p);
+console.log('p.getName..', p.getName());
