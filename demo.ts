8c96f1a60212160fbaf564bab7ad43f34c6b8a4e
unknown <2900170896@qq.com>
2021-04-06 19:44:44 +0800

    stash

--- demo.ts
+++ demo.ts
@@ -1,31 +1,62 @@
-const userInfo: any = undefined;
-
-function catchError(name: string){
-    return function(target: any, key: string, descriptor: PropertyDescriptor){
-        const fn = descriptor.value;
-        descriptor.value = function(){
-            try {
-                fn();
-            } catch (e) {
-                console.log(`userInfo.${name}存在问题..`)
-            }
-        }
-    }
+interface Teacher {
+    name: string;
+    age: number;
 }
 
-class Person {
-
-    @catchError('name')
-    getName(){
-        return userInfo.name;
-    }
-
-    @catchError('age')
-    getAge(){
-        return userInfo.age;
-    }
+interface Student {
+    name: string;
+    age: number;
+    score: number;
 }
 
-const p = new Person();
-p.getName();
-p.getAge();
+// 1.交叉类型
+type Test = Teacher & Student;
+const printObj = (obj: Test): string => {
+    console.log(obj);
+    return obj.name;
+};
+// 传入的参数必须同时包含Teacher和Student里面的属性。
+const p: Teacher & Student = {
+    name: 'lzc',
+    age: 23,
+    score: 43
+}
+printObj(p)
+
+// 2. typeof 关键字
+const printT = typeof printObj;
+const teacherT = typeof p;
+console.log(printT);
+
+// 3. keyof关键字
+type k1 = keyof Teacher;
+const k: k1 = 'age'; // k只能取值 'name'或者 'age'
+
+type K2 = keyof Teacher[];
+const k2: K2 = 'entries'; // k2只能是number类型或者是Array.protoype上的属性
+
+interface P2 {
+    [x: string]: Teacher;
+}
+type K3 = keyof P2;
+const n: K3 = '43';
+
+interface StringArray {
+    [index: number]: string;
+}
+
+const myArray: StringArray = {}
+
+myArray[0] = 'fdaf';
+
+// 接口定义构造函数
+interface IClass {
+    new (hour: number, minute: number): void;
+}
+
+const test2: IClass = class {
+    constructor(hour: number){
+
+    }
+}
+const t = new test2(4);
